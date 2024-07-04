e1fda67daa9cfda8df52cb24abc10417f87ca70a
Selman Aslan <selman.aslan@roofstacks.com>
2024-07-04 15:28:13 +0300

    Add transparency and blur effect to TabBarView
    Modified TabBarView to include a transparent white background for the tab bar using CupertinoColors.white.withOpacity(0.8). Added CupertinoPageScaffold with a slightly transparent background (CupertinoColors.white.withOpacity(0.5)) to create a blur effect for each tab.

--- lib/Presentation/TabBar/TabBarView.dart
+++ lib/Presentation/TabBar/TabBarView.dart
@@ -1,11 +1,10 @@
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:rmcharactersappfluttercupertino/Presentation/FavoritedCharacters/FavoritedCharactersView.dart';
 import 'package:rmcharactersappfluttercupertino/Presentation/RMCharacters/RMCharactersView.dart';
 
-
 class TabBarView extends StatefulWidget {
   const TabBarView({super.key});
-
 
   @override
   State<TabBarView> createState() => _TabBarViewState();
@@ -15,37 +14,37 @@ class _TabBarViewState extends State<TabBarView> {
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-          border: null,
-          backgroundColor: CupertinoColors.white,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.heart_fill),
-              label: 'Favorites',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.person_2_fill),
-              label: 'Characters',
-            ),
-          ],
-        ),
-        tabBuilder: (BuildContext context, int index) {
-          Widget tab;
-          switch (index) {
-            case 0:
-              tab = FavoritedCharactersView();
-              break;
-            case 1:
-              tab = RMCharactersView();
-              break;
-            default:
-              tab = FavoritedCharactersView();
-          }
-          return tab;
-        },
-        backgroundColor: CupertinoColors.white,
-      );
+      tabBar: CupertinoTabBar(
+        border: null,
+        backgroundColor: CupertinoColors.white.withOpacity(0.8),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.heart_fill),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person_2_fill),
+            label: 'Characters',
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        Widget tab;
+        switch (index) {
+          case 0:
+            tab = FavoritedCharactersView();
+            break;
+          case 1:
+            tab = RMCharactersView();
+            break;
+          default:
+            tab = FavoritedCharactersView();
+        }
+        return CupertinoPageScaffold(
+          backgroundColor: CupertinoColors.white.withOpacity(0.5),
+          child: tab,
+        );
+      },
+    );
   }
 }
-
-
